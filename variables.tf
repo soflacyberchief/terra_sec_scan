fb26047b1b8b43bfbc36da012f8622e62b271db9
soflacyberchief <62037720+soflacyberchief@users.noreply.github.com>
2021-03-07 22:21:39 -0500

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "AWS region where the customer needs to be deployed"
-  default     = "us-west-2"
-}
-
-//Customer Account AWS Profile
-variable "ce_account_profile" {
-  description = "AWS Profile name for Customer Environment"
-}
