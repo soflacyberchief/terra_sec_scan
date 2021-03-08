fd5d6c567cca7dc8d01d9c160a02e0d3ada052ee
soflacyberchief <62037720+soflacyberchief@users.noreply.github.com>
2021-03-07 22:21:31 -0500

    Delete state.tf

--- state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket     = "my-terraform-state-bucket"
-    key        = "prod/terraform.tfstate"
-    region     = "us-west-2"
-    access_key = "AKIAIOSFODNN8EXAMPLE"
-    secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYBLAHKEY"
-  }
-}
